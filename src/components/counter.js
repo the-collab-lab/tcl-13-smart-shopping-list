176dad600fde53410eb0228a8830a9cc20ec1f18
rachel fischoff <59700665+rachel-fischoff@users.noreply.github.com>
2020-10-07 19:16:51 -0400

    with counter imported into App.js

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
+import db from '../firebase/firebase';
 
 export default class counter extends Component {
   constructor() {
     super();
+    //create state with a counter set to zero
     this.state = { currentCount: 0 };
   }
 
-  handleChange = (event) => {};
+  //event handler function that adds count to state when the button is clicked
+  handleChange = (event) => {
+    event.preventDefault();
+
+    this.setState((prevState) => ({ counter: prevState.counter++ }));
+
+    console.log(this.state.counter, 'the current state of the counter');
+  };
 
   render() {
     return (
       <div>
-        <button>count</button>
+        <button onClick={this.handleChange}>count</button>
       </div>
     );
   }
