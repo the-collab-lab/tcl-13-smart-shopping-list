19a6ac051ad006b6c1938e7dffca448c689b99c1
Shayne Sebro <shayne@shaynes-mbp.attlocal.net>
2020-10-07 19:32:29 -0400

    added counter component to show db connection on the frontend

--- src/components/counter.js
+++ src/components/counter.js
@@ -5,10 +5,12 @@ export default class Counter extends Component {
   constructor() {
     super();
     //create state with a counter set to zero
-    this.state = { currentCount: 0 };
+    this.state = {
+      currentCount: 0,
+    };
   }
 
-  //event handler function that adds count to state when the button is clicked
+  //event handler function that adds count to state when the button is clicked, and saves count to databse
   handleChange = (event) => {
     event.preventDefault();
 
@@ -17,12 +19,39 @@ export default class Counter extends Component {
     }));
 
     console.log(this.state.currentCount, 'the current state of the counter');
+
+    //Writing to DB
+    db.collection('counter')
+      .doc('count')
+      .set({
+        number: this.state.currentCount,
+      })
+      .then(function () {
+        console.log('updated count!');
+      })
+      // catches & logs any errors
+      .catch(function (error) {
+        console.error('error updating count!', error);
+      });
+  };
+
+  // event handler to read current count from database
+  handleGet = (e) => {
+    db.collection('counter')
+      .doc('count')
+      .get()
+      .then((doc) => {
+        const data = doc.data();
+        console.log('current count in database is', data);
+      });
   };
 
   render() {
     return (
       <div>
-        <button onClick={this.handleChange}> count</button>
+        <button onClick={this.handleChange}>Count</button>
+
+        <button onClick={this.handleGet}>Get Count</button>
       </div>
     );
   }
