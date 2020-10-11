db1bb7b0bb291d367ad7537eb1f48305b1cc9779
rachel fischoff <59700665+rachel-fischoff@users.noreply.github.com>
2020-10-11 18:24:17 -0400

    added add-item input type submit

--- src/components/AddItems.js
+++ src/components/AddItems.js
@@ -6,25 +6,25 @@ const AddItems = () => {
     <div>
       <h1>Add your items!</h1>
       <form>
-        <label for="item-name"> Item Name:</label>
+        <label htmlFor="item-name"> Item Name:</label>
         <input type="text" />
         <br />
         <br />
         <fieldset>
           <legend>Time Frame</legend>
-          <label for="time-frame"> How soon will you buy this again?</label>
+          <label htmlFor="time-frame"> How soon will you buy this again?</label>
           <br />
-          <input type="radio" id="Soon" name="time-frame" />
-          <label for="soon"> Soon</label>
+          <input type="radio" id="Soon" name="time-frame" checked />
+          <label htmlFor="soon"> Soon</label>
           <br />
-
           <input type="radio" id="Kinda Soon" name="time-frame" />
-          <label for="kinda-soon"> Kinda Soon</label>
+          <label htmlFor="kinda-soon"> Kinda Soon</label>
           <br />
           <input type="radio" id="Not Soon" name="time-frame" />
-          <label for="not-soon">Not Soon</label>
+          <label htmlFor="not-soon">Not Soon</label>
           <br />
         </fieldset>
+        <input type="submit" value="Add Item" />
       </form>
     </div>
   );
