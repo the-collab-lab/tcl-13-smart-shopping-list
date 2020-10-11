e143c2cfbef2dbe27e6d72370746f0044ddd8be6
rachel fischoff <59700665+rachel-fischoff@users.noreply.github.com>
2020-10-11 18:12:17 -0400

    added form element with input for text and radio buttons to AddItems.js

--- src/components/AddItems.js
+++ src/components/AddItems.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import db from '../firebase/firebase';
 
 const AddItems = () => {
   return (
     <div>
       <h1>Add your items!</h1>
-      <form></form>
+      <form>
+        <label for="item-name"> Item Name:</label>
+        <input type="text" />
+        <br />
+        <br />
+        <fieldset>
+          <legend>Time Frame</legend>
+          <label for="time-frame"> How soon will you buy this again?</label>
+          <br />
+          <input type="radio" id="Soon" name="time-frame" />
+          <label for="soon"> Soon</label>
+          <br />
+
+          <input type="radio" id="Kinda Soon" name="time-frame" />
+          <label for="kinda-soon"> Kinda Soon</label>
+          <br />
+          <input type="radio" id="Not Soon" name="time-frame" />
+          <label for="not-soon">Not Soon</label>
+          <br />
+        </fieldset>
+      </form>
     </div>
   );
 };
