4dd4b5797f4c3b434ca554b451aa7cd0f373c2d7
Audra T <audraturuta@gmail.com>
2020-10-17 17:05:17 -0400

    App checks for token, displays view list if a token is found, displays the welcome screen if not

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Nav from './components/Nav';
 import Router from './components/Router';
+import CheckToken from './components/CheckToken';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <header className="App-header">
           <h1>Welcome to your Shopping List!</h1>
         </header>
+        <CheckToken />
         <Router />
         <Nav />
       </div>
