552f3a601352e524e159ddfd5902e5a46b65d1c9
Shayne Sebro <shaynesebro@gmail.com>
2020-10-26 18:57:11 -0400

    trying to work with firebase docs and update methods

--- src/components/ViewList.js
+++ src/components/ViewList.js
@@ -1,8 +1,46 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { ListContext } from '../context/ListContext';
+import { db } from '../lib/firebase';
 
 const ViewList = () => {
   let currentList = useContext(ListContext);
+  let token = currentList.token;
+
+  const [itemsPurchased, setItemsPurchased] = useState('');
+  const [checked, setChecked] = useState(false);
+
+  const handleCheck = (e) => {
+    e.preventDefault();
+
+    setItemsPurchased(e.target.value);
+    setChecked(true);
+
+    // currentList.itemsRef
+    //   .where('userToken', '==', localStorage.getItem('tcl13-token'))
+    //   .where('itemName', '==', itemsPurchased)
+    //   .set({ lastPurchased: new Date() }, {merge: true})
+    //   .then(function () {
+    //     console.log('Document Updated Succesfully');
+    //   })
+    //   .catch(function (error) {
+    //     console.error('Error Updating Document');
+    //   });
+
+    currentList.itemsRef.where('userToken', '==', token).onSnapshot(
+      (querySnapshot) => {
+        let tempItem = querySnapshot.docs;
+
+        let updateItem = tempItem.filter(
+          (item) => item.data().itemName === itemsPurchased,
+        );
+
+        console.log(updateItem);
+      },
+      (error) => {
+        console.log('Error getting documents: ', error);
+      },
+    );
+  };
 
   return (
     <div>
@@ -10,7 +48,17 @@ const ViewList = () => {
       <ul>
         {currentList.userList &&
           currentList.userList.map((element, index) => (
-            <li key={index}> {element.itemName} </li>
+            <div key={index}>
+              <input
+                type="checkbox"
+                name={element.itemName}
+                value={element.itemName}
+                className="purchased"
+                onChange={handleCheck}
+                checked={checked}
+              ></input>
+              <li> {element.itemName} </li>
+            </div>
           ))}
       </ul>
     </div>
