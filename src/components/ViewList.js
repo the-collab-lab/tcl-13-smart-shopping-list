b1560aa557416861670eb02e3702478c28b7b6c2
rachel fischoff <59700665+rachel-fischoff@users.noreply.github.com>
2020-10-27 18:55:06 -0400

    fix: checkbox updating item in state & database

--- src/components/ViewList.js
+++ src/components/ViewList.js
@@ -1,26 +1,35 @@
 import React, { useContext, useState } from 'react';
 import { ListContext } from '../context/ListContext';
-import { db } from '../lib/firebase';
 
 const ViewList = () => {
   let currentList = useContext(ListContext);
   let token = currentList.token;
 
-  const [itemsPurchased, setItemsPurchased] = useState('');
-  const [checked, setChecked] = useState(false);
+  const [itemsPurchased, setItemsPurchased] = useState({});
 
   const handleCheck = (e) => {
-    setItemsPurchased(e.target.value);
-    setChecked(true);
+    const item = e.target.name;
+    const isChecked = e.target.checked;
+    setItemsPurchased((prevItemsPurchased) => ({
+      ...prevItemsPurchased,
+      [item]: isChecked,
+    }));
+  };
 
+  const updateDatabase = (e) => {
+    handleCheck(e);
+    //finds the current list
     currentList.itemsRef.where('userToken', '==', token).onSnapshot(
       function (querySnapShot) {
         querySnapShot.forEach(function (doc) {
+          //finds the documents where the itemsPurchased exist
           if (doc.data().itemName === itemsPurchased) {
+            //grabs the id
             const itemId = doc.id;
+            //finds the document & sets the lastPurchased field to current date
             currentList.itemsRef
               .doc(itemId)
-              .set({ lastPurchased: new Date() }, { merge: true });
+              .update({ lastPurchased: new Date() });
           }
         });
       },
@@ -42,8 +51,8 @@ const ViewList = () => {
                 name={element.itemName}
                 value={element.itemName}
                 className="purchased"
-                onChange={handleCheck}
-                checked={checked}
+                onChange={(e) => updateDatabase(e)}
+                checked={itemsPurchased[element.itemName] || false}
               ></input>
               <li> {element.itemName} </li>
             </div>
